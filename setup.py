e6ca56ed9edc01f2f30c6e4074fdfe8672b7fb3c
Eirik <serexat292@bulmp3.com>
2025-07-04 17:38:38 -0400

    Update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='project_alpha',
+    name='alpha',
     version='0.2.0',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
